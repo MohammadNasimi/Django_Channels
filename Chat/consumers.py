f2aa104dd5355136e0a69fff9a60a7a0b63697dc
MohammadNasimi <mohammadnasimi6803@gmail.com>
2024-06-26 23:00:02 +0330

    complete fetch_message user login

--- Chat/consumers.py
+++ Chat/consumers.py
@@ -2,14 +2,26 @@ import json
 
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
-
+from .serializers import MessageSerializer
+from .models import Message
+from rest_framework.renderers import JSONRenderer
 
 class ChatConsumer(WebsocketConsumer):
     
     def new_message(self,message):
         print(message)
-    def fetch_message(self):
-         pass
+    def fetch_message(self,data):
+        query = Message.last_message(self)
+        messge_json = self.message_serializer(query)
+        content ={
+            "message":eval(messge_json)
+        } 
+        self.chat_message(content)
+     
+    def message_serializer(self,query_set):
+        serialized = MessageSerializer(query_set,many=True)
+        content = JSONRenderer().render(serialized.data)
+        return content
     
     def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"] # info connection save in scope like request in api 
@@ -36,11 +48,11 @@ class ChatConsumer(WebsocketConsumer):
     # Receive message from WebSocket
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
+        message = text_data_json.get("message",None)
         commad = text_data_json["commad"]
         self.commends[commad](self,message)
 
-
+    def send_to_chat_message(self,message):
         # Send message to room group
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name, {"type": "chat_message", "message": message}
@@ -55,6 +67,5 @@ class ChatConsumer(WebsocketConsumer):
         }
         """
         message = event["message"]
-
         # Send message to WebSocket
         self.send(text_data=json.dumps({"message": message}))
