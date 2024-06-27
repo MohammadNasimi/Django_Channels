43048aa997f1be6d41fe492da96e75fe3b12dc39
MohammadNasimi <mohammadnasimi6803@gmail.com>
2024-06-27 23:39:18 +0330

    add url log in

--- Chat/urls.py
+++ Chat/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
 from . import views
-
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("<str:room_name>/", views.room, name="room"),
-
+    path('login', LoginView.as_view(template_name='chat/login.html'), name="login")
 ]
